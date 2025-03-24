e57995814cf572345e717be1c81492ed2e9be621
thektrnesis <408775@niuitmo.ru>
2025-03-24 11:51:11 +0300

    rough solution

--- internal/usecase/sensor.go
+++ internal/usecase/sensor.go
@@ -6,24 +6,45 @@ import (
 )
 
 type Sensor struct {
-	// TODO добавьте реализацию
+	sensorRepo SensorRepository
 }
 
 func NewSensor(sr SensorRepository) *Sensor {
-	return &Sensor{}
+	return &Sensor{
+		sensorRepo: sr,
+	}
 }
 
 func (s *Sensor) RegisterSensor(ctx context.Context, sensor *domain.Sensor) (*domain.Sensor, error) {
-	// TODO добавьте реализацию
-	return nil, nil
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+
+	return sensor, s.sensorRepo.SaveSensor(ctx, sensor)
 }
 
 func (s *Sensor) GetSensors(ctx context.Context) ([]domain.Sensor, error) {
-	// TODO добавьте реализацию
-	return nil, nil
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+
+	sensors, err := s.sensorRepo.GetSensors(ctx)
+
+	return sensors, err
 }
 
 func (s *Sensor) GetSensorByID(ctx context.Context, id int64) (*domain.Sensor, error) {
-	// TODO добавьте реализацию
-	return nil, nil
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+
+	sensor, err := s.sensorRepo.GetSensorByID(ctx, id)
+
+	return sensor, err
 }
