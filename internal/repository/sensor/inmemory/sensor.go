95bdd98b174bae0976f2986d202c818997740ecd
thektrnesis <408775@niuitmo.ru>
2025-03-24 16:04:01 +0300

    hw 6: tests passed (except ctx.deadline)

--- internal/repository/sensor/inmemory/sensor.go
+++ internal/repository/sensor/inmemory/sensor.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"homework/internal/domain"
+	"homework/internal/usecase"
 	"sync"
+	"time"
 )
 
 type SensorRepository struct {
@@ -21,6 +23,9 @@ func NewSensorRepository() *SensorRepository {
 }
 
 func (r *SensorRepository) SaveSensor(ctx context.Context, sensor *domain.Sensor) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
 	if sensor == nil {
 		return fmt.Errorf("sensor is nil")
 	}
@@ -28,10 +33,17 @@ func (r *SensorRepository) SaveSensor(ctx context.Context, sensor *domain.Sensor
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	if _, ok := r.sensorsById[sensor.ID]; ok {
-		return nil
+	if len(sensor.SerialNumber) != 10 {
+		return fmt.Errorf("sensor serial number is invalid")
+	}
+	if sensor.Type != domain.SensorTypeContactClosure && sensor.Type != domain.SensorTypeADC {
+		return fmt.Errorf("sensor type is invalid")
 	}
 
+	if _, ok := r.sensorsBySerialNumber[sensor.SerialNumber]; ok {
+		return nil
+	}
+	sensor.RegisteredAt = time.Now()
 	r.sensorsById[sensor.ID] = sensor
 	r.sensorsBySerialNumber[sensor.SerialNumber] = sensor
 
@@ -39,36 +51,48 @@ func (r *SensorRepository) SaveSensor(ctx context.Context, sensor *domain.Sensor
 }
 
 func (r *SensorRepository) GetSensors(ctx context.Context) ([]domain.Sensor, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
 	sensors := make([]domain.Sensor, 0, len(r.sensorsById))
-	for _, sensor := range r.sensorsById {
+	for _, sensor := range r.sensorsBySerialNumber {
 		sensors = append(sensors, *sensor)
 	}
 
-	return nil, nil
+	return sensors, nil
 }
 
 func (r *SensorRepository) GetSensorByID(ctx context.Context, id int64) (*domain.Sensor, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
 	sensor, ok := r.sensorsById[id]
 	if !ok {
-		return nil, fmt.Errorf("sensor with id %d not found", id)
+		return nil, fmt.Errorf("sensor with id %d not found: %w", id, usecase.ErrSensorNotFound)
 	}
 
 	return sensor, nil
 }
 
 func (r *SensorRepository) GetSensorBySerialNumber(ctx context.Context, sn string) (*domain.Sensor, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
 	sensor, ok := r.sensorsBySerialNumber[sn]
 	if !ok {
-		return nil, fmt.Errorf("sensor with serial number %s not found", sn)
+		return nil, fmt.Errorf("sensor with serial number %s not found: %w", sn, usecase.ErrSensorNotFound)
 	}
 
 	return sensor, nil
